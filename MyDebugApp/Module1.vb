9409d563f5c9cd31c2803811d7ec9473b874f0f8
Hjälte <hjalte79@users.noreply.github.com>
2022-05-15 09:48:20 +0200

    Added Debug project

---
+++ MyDebugApp/Module1.vb
@@ -0,0 +1,15 @@
+﻿Imports Inventor
+Imports System.Runtime.InteropServices
+Imports MyILogicAddin ' This is the namespace from the addin dll!
+
+Module Module1
+
+    Sub Main()
+        Dim inventorObject As Inventor.Application = Marshal.GetActiveObject("Inventor.Application")
+
+        Dim rule As New ThisRule()
+        rule.ThisApplication = inventorObject
+        rule.Main()
+    End Sub
+
+End Module
